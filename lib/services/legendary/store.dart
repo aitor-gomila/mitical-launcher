479654298b7719994f95b887a57f054c006f532f
aitor-gomila <aitor-gomila@outlook.com>
2023-07-06 14:01:02 +0200

    Provide built-in legendary executable

--- lib/services/legendary/store.dart
+++ lib/services/legendary/store.dart
@@ -1,4 +1,22 @@
+import 'dart:io';
+import 'package:path/path.dart' as path;
+
+enum LegendaryStoreValue<T> { none, loading, error<Error>(), value<T>() }
+
 class LegendaryStore {
+  // Default platform name e.g. windows, mac and linux
+  static final String _platformName = Platform.isWindows
+      ? "windows"
+      : Platform.isMacOS
+          ? "darwin"
+          : Platform.isLinux
+              ? "linux"
+              : "";
+
+  // default path to legendary, e.g. assets/legendary/linux/legendary or assets/legendary/windows/legendary.exe
+  static final String _defaultLegendaryPath = path.join("assets", "legendary",
+      _platformName, Platform.isWindows ? "legendary.exe" : "legendary");
+
   final String legendaryPath;
 
   Map<String, dynamic> toJson() {
@@ -10,12 +28,9 @@ class LegendaryStore {
   factory LegendaryStore.fromJson(Map<String, dynamic> json) {
     final legendaryPath = json["legendaryPath"];
 
-    return LegendaryStore(
-      legendaryPath: legendaryPath
-    );
+    return LegendaryStore(legendaryPath: legendaryPath);
   }
 
-  LegendaryStore({
-    required this.legendaryPath
-  });
+  LegendaryStore({String? legendaryPath})
+      : legendaryPath = legendaryPath ?? _defaultLegendaryPath;
 }
